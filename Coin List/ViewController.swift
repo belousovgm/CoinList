1ddc0a5db3598cce4990c0a624a28fe5a940ab9e
Grigory Belousov <gbelousov@icerockdev.com>
2022-09-20 17:06:48 +0400

    Initial Commit

---
+++ Coin List/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Coin List
+//
+//  Created by Grigory Belousov on 20.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
