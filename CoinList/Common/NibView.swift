4f7e39490303afac6ee3a2135837d029c5599cf4
Grigory Belousov <gbelousov@icerockdev.com>
2022-09-23 22:25:42 +0400

    add base view for nib loading views

---
+++ CoinList/Common/NibView.swift
@@ -0,0 +1,37 @@
+//
+//  NibView.swift
+//  CoinList
+//
+//  Created by Grigory Belousov on 23.09.2022.
+//
+
+import Foundation
+import UIKit
+
+class NibView: UIView {
+    public static var nibName: String {
+        return String(describing: Self.self)
+    }
+    
+    public static var nib: UINib {
+        let bundle = Bundle(for: Self.self)
+        return UINib(nibName: Self.nibName, bundle: bundle)
+    }
+    
+    func setupFromNib() {
+        guard let view = Self.nib.instantiate(withOwner: self, options: nil).first as? UIView else {
+            fatalError("Error loading \(self) from nib")
+        }
+        
+        addSubview(view)
+        
+        view.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            view.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: 0),
+            view.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 0),
+            view.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor, constant: 0),
+            view.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor, constant: 0)
+        ])
+    }
+}
